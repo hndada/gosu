61701feb76bcf9cf209c8061a83fe1089cddcb23
hndada <33769671+hndada@users.noreply.github.com>
2020-09-21 01:08:52 +0900

    add remained memo

--- select.go
+++ select.go
@@ -21,11 +21,18 @@ import (
 // todo: 로딩일 때 기다리는 로직
 // Loading 이라는 별도의 Lock을 둔 이상, 특별히 채널은 필요없는거 아닌가?
 // 비트맵 로딩 15초 후 timeout
+
+// chart panel에 저장할 것:
+// timing point 제외 basechart 전부 (차트 기본 정보)
+// 난이도 계산된 값 (겉값)
+// 채보 속성
+// 채보 파일 경로
 type SceneSelect struct {
 	g      *Game
 	charts []chartPanel
 	cursor int
 	mods   mania.Mods
+	hold   bool
 	// 그룹 (디렉토리 트리)
 	// 현재 정렬 기준
 	// Buttons     []ebitenui.Button
@@ -49,6 +56,27 @@ func (g *Game) NewSceneSelect() *SceneSelect {
 // +시프트: 그룹 이동
 func (s *SceneSelect) Init() {}
 func (s *SceneSelect) Update() error {
+	// if !s.hold{
+	// 	switch {
+	// 	case ebiten.IsKeyPressed(ebiten.KeyEnter):
+	// 		s.g.changeScene(mania.NewScene(s.charts[s.cursor].chart, s.mods))
+	// 		s.hold = true
+	// 	case ebiten.IsKeyPressed(ebiten.KeyDown):
+	// 		s.cursor++
+	// 		if s.cursor >= len(s.charts) {
+	// 			s.cursor = 0
+	// 		}
+	// 		s.hold = true
+	// 	case ebiten.IsKeyPressed(ebiten.KeyUp):
+	// 		s.cursor--
+	// 		if s.cursor < 0 {
+	// 			s.cursor = len(s.charts) - 1
+	// 		}
+	// 		s.hold = true
+	// 	default:
+	// 		s.hold = false
+	// 	}
+	// }
 	if ebiten.IsKeyPressed(ebiten.KeyEnter) {
 		s.g.changeScene(mania.NewScene(s.charts[s.cursor].chart, s.mods))
 	}
@@ -64,6 +92,7 @@ func (s *SceneSelect) Update() error {
 			s.cursor = len(s.charts) - 1
 		}
 	}
+
 	screenSize := game.ScreenSize()
 	for i := range s.charts {
 		mid := (screenSize.Y - 40) / 2 // 현재 선택된 차트 focus 틀 위치 고정
@@ -156,7 +185,7 @@ func newChartPanel(c *mania.Chart) chartPanel {
 		Face: basicfont.Face7x13,
 		Dot:  point,
 	}
-	d.DrawString(fmt.Sprintf("%s [%s]", c.MusicName, c.ChartName))
+	d.DrawString(fmt.Sprintf("(%dKey Lv.%.2f) %s [%s]", c.Keys, c.Level, c.MusicName, c.ChartName))
 	cp.box, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
 	cp.op = &ebiten.DrawImageOptions{}
 	cp.chart = c
