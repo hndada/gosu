46d6b841618c512c3eaa1fd2c6e002c36f0823b5
Muang <33769671+hndada@users.noreply.github.com>
2021-04-12 15:54:44 +0900

    revive gosu-calc

---
+++ cmd/calc/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+
+	"github.com/hndada/gosu/game/mania"
+	"github.com/hndada/rg-parser/osugame/osu"
+)
+
+func main() {
+	const dirname = "./charts"
+	files, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		s := filepath.Join(dirname, f.Name())
+		o, err := osu.Parse(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c, _ := mania.NewChartFromOsu(o, s)
+		c.CalcDifficulty()
+		fmt.Printf("%3.3f: %s\n", c.Level/25, f.Name())
+	}
+}
