f74d1ae93d5812a050b5ada5014d07f0d594590b
Muang <33769671+hndada@users.noreply.github.com>
2021-04-12 22:35:03 +0900

    clean comments up

--- game.go
+++ game.go
@@ -9,19 +9,9 @@ import (
 	"path/filepath"
 )
 
-// select에서 곡 로드 방식 바꾸기, 그 외 간단히 고칠 것들 todo 나 메모에서
-// 리플레이 저장, 이후 스코어/hp 고치기
-// 기타 Sprite 그리기
-// docs, markdown 정리 및 internal 삭제
-
-// 운동, 코딩, 잡일, 독서, 글씨 연습
-// 다른 곳으로 날아가도, 외출 때 코딩을 하겠음, 아니면 과외나 학원 알바를 하든가
-
-// 레벨 튜닝
-// 시스템 디자인: pp(그대로 갈듯), 심플 웹, 랭크 시스템, 채보 discussion and contribution
-// ui
-// gosu만의 특별한 기능: 다른 파일 포맷 파싱 등
-// game programming beginner's guide with go?
+// 스코어, hp 고치기
+// internal 삭제
+// pp
 var MaxTransCountDown int
 
 type Game struct {
