df1a8e857c4429ab574e84b3c51d57e931ea5e40
hndada <hndada@dgist.ac.kr>
2021-08-29 06:21:44 +0000

    keystroke listener done

--- cmd/keyboard/main.go
+++ cmd/keyboard/main.go
@@ -4,27 +4,46 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/eiannone/keyboard"
+	"golang.org/x/sys/windows"
 )
 
-func main() {
-	if err := keyboard.Open(); err != nil {
-		panic(err)
-	}
-	defer func() {
-		_ = keyboard.Close()
-	}()
+var (
+	moduser32            = windows.NewLazyDLL("user32.dll")
+	procGetAsyncKeyState = moduser32.NewProc("GetAsyncKeyState")
+)
 
-	fmt.Println("Press ESC to quit")
+const (
+	WasPressed = 0x0001 // deprecated: whether the key was pressed after the previous call to GetAsyncKeyState
+	IsPressed  = 0x8000
+)
+
+var lastPressed [256]bool
+
+func main() {
+	startTime := time.Now()
 	for {
-		char, key, err := keyboard.GetKey()
-		if err != nil {
-			panic(err)
+		for i := 0; i < 0xFF; i++ { // Query key mapped to integer `0x00` to `0xFF` if it's pressed.
+			keyCode := convVirtualKeyCode(uint32(i))
+			if keyCode == 0 || keyCode == CodeUnknown {
+				continue
+			}
+			v, _, _ := procGetAsyncKeyState.Call(uintptr(i))
+			switch {
+			case v&IsPressed != 0 && !lastPressed[i]:
+				fmt.Printf("%s pressed at %vms\n", keyCode, time.Since(startTime).Milliseconds())
+				lastPressed[i] = true
+			case v&IsPressed == 0 && lastPressed[i]:
+				fmt.Printf("%s released at %vms\n", keyCode, time.Since(startTime).Milliseconds())
+				lastPressed[i] = false
+			}
 		}
-		t:=time.Now()
-		fmt.Printf("You pressed: rune %q, key %X at time: %+v\r\n", char, key, t)
-		if key == keyboard.KeyEsc {
-			break
-		}
+		time.Sleep(1 * time.Microsecond) // prevents 100% CPU usage
 	}
 }
+
+// const (
+// 	Idle    = 0x0000
+// 	Release = WasPressed + Idle
+// 	Press   = Idle + IsPressed
+// 	Hold    = WasPressed + IsPressed
+// )
