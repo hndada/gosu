089d05308aac7421193fe25755d0118ae74e231a
Muang <hndada@dgist.ac.kr>
2022-08-09 19:17:26 +0900

    playing sample sound of note in progress

--- note.go
+++ note.go
@@ -18,8 +18,6 @@ const (
 	Tail            = longNote + release
 )
 
-// Prev와 Next는 Note 내에 있으면 안될 것 같다. Scene 등에서 즉석에서 생성 및 관리해야지.
-// Prev int // Next note's index
 type Note struct {
 	Type           NoteType
 	Time           int64
@@ -29,21 +27,21 @@ type Note struct {
 	SampleVolume   int
 }
 
-// Samples should be lazy loaded
+// A sample sound file should be lazy loaded.
 func NewNoteFromOsu(ho osu.HitObject, keyCount int) []Note {
 	ns := make([]Note, 0, 2)
 	n := Note{
-		Type:  Normal,
-		Time:  int64(ho.Time),
-		Time2: int64(ho.Time),
-		Key:   ho.Column(keyCount),
-		// SampleFilename: ,
-		// SampleVolume: ,
+		Type:           Normal,
+		Time:           int64(ho.Time),
+		Time2:          int64(ho.Time),
+		Key:            ho.Column(keyCount),
+		SampleFilename: ho.HitSample.Filename,
+		SampleVolume:   ho.HitSample.Volume,
 	}
 	if ho.NoteType&osu.ComboMask == osu.HitTypeHoldNote {
 		n.Type = Head
 		n.Time2 = int64(ho.EndTime)
-		n2 := Note{ // Tail has no sample sound
+		n2 := Note{ // Tail has no sample sound.
 			Type:  Tail,
 			Time:  n.Time2,
 			Time2: n.Time,
