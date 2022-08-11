e8173c5a250b301039ab17003ce59fe54e294c94
Muang <hndada@dgist.ac.kr>
2022-08-11 16:01:52 +0900

    implement efficient long note drawing
    1248234 Taishi - bluefieldcreator has been tested

--- playnote.go
+++ playnote.go
@@ -7,16 +7,18 @@ import (
 // PlayNote is for in-game. Handled by pointers to modify its fields easily.
 type PlayNote struct {
 	Note
-	Prev   *PlayNote
-	Next   *PlayNote
-	Scored bool
+	Prev     *PlayNote
+	Next     *PlayNote
+	Scored   bool
+	NextTail *PlayNote // For performance of DrawLongNotes()
 }
 
-// Second output is initial staged notes.
-func NewPlayNotes(c *Chart) (playNotes []*PlayNote, stagedNotes []*PlayNote) {
-	playNotes = make([]*PlayNote, 0, len(c.Notes))
-	stagedNotes = make([]*PlayNote, c.KeyCount)
+func NewPlayNotes(c *Chart) ([]*PlayNote, []*PlayNote, []*PlayNote) {
+	playNotes := make([]*PlayNote, 0, len(c.Notes))
+	firstStagedNotes := make([]*PlayNote, c.KeyCount)
+	firstLowestTails := make([]*PlayNote, c.KeyCount)
 	prevs := make([]*PlayNote, c.KeyCount)
+	prevTails := make([]*PlayNote, c.KeyCount)
 	for _, n := range c.Notes {
 		prev := prevs[n.Key]
 		pn := &PlayNote{
@@ -27,50 +29,59 @@ func NewPlayNotes(c *Chart) (playNotes []*PlayNote, stagedNotes []*PlayNote) {
 			prev.Next = pn
 		}
 		prevs[n.Key] = pn
-		if stagedNotes[n.Key] == nil {
-			stagedNotes[n.Key] = pn
+		if firstStagedNotes[n.Key] == nil {
+			firstStagedNotes[n.Key] = pn
+		}
+		if n.Type == Tail {
+			if prevTails[n.Key] != nil {
+				prevTails[n.Key].NextTail = pn
+			}
+			prevTails[n.Key] = pn
+			if firstLowestTails[n.Key] == nil {
+				firstLowestTails[n.Key] = pn
+			}
 		}
 		playNotes = append(playNotes, pn)
 	}
-	return
+	return playNotes, firstStagedNotes, firstLowestTails
 }
 
-// DrawLongNote draws long note before drawing Head or Tail.
-func (s *ScenePlay) DrawLongNotes(screen *ebiten.Image) {
-	for _, n := range s.PlayNotes {
-		if n.Type != Tail {
-			continue
-		}
-		// Gives extra 1 pixel for compensating round-down
-		top := int(s.NotePosition(n)+s.TailSprites[n.Key].H/2) - 1
-		bottom := int(s.NotePosition(n.Prev)-s.HeadSprites[n.Key].H/2) + 1
-		if top > screenSizeY || bottom < 0 {
-			continue
-		}
-		if top < 0 {
-			top = 0
-		}
-		if bottom > screenSizeY {
-			bottom = screenSizeY
-		}
+// top returns top position of long note body.
+func (s ScenePlay) Top(tail *PlayNote) int {
+	return int(s.NotePosition(tail)+s.TailSprites[tail.Key].H/2) - 1 // Extra 1 pixel for compensating round-down
+}
 
-		sprite := s.BodySprites[n.Key]
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Scale(sprite.ScaleW(), sprite.ScaleH())
-		op.GeoM.Translate(sprite.X, float64(top))
-		if n.Scored {
-			op.ColorM.ChangeHSV(0, 0.3, 0.3)
+// bottom returns bottom position of long note body.
+func (s ScenePlay) Bottom(tail *PlayNote) int {
+	return int(s.NotePosition(tail.Prev)-s.HeadSprites[tail.Key].H/2) + 1 // Extra 1 pixel for compensating round-down
+}
+
+// DrawLongNotes draws long note before drawing Head or Tail.
+// DrawLongNotes just draws sub image of long note body.
+func (s *ScenePlay) DrawLongNotes(screen *ebiten.Image) {
+	for k, n0 := range s.LowestTails {
+		for n := n0; n != nil && s.Top(n) >= screenSizeY; n = n.NextTail {
+			s.LowestTails[k] = n
 		}
-		l, h := bottom-top, int(sprite.H) // Bottom has larger value
-		q, r := l/h, l%h
-		for i := 0; i < q; i++ {
-			screen.DrawImage(sprite.I, op)
-			op.GeoM.Translate(0, float64(h))
+		for n := s.LowestTails[k]; n != nil && s.Bottom(n) >= 0; n = n.NextTail {
+			top := s.Top(n)
+			bottom := s.Bottom(n)
+			if top < 0 {
+				top = 0
+			}
+			if bottom > screenSizeY {
+				bottom = screenSizeY
+			}
+			sprite := s.BodySprites[k]
+			sprite.Y = float64(top)
+			op := sprite.Op()
+			if n.Scored {
+				op.ColorM.ChangeHSV(0, 0.3, 0.3)
+			}
+			rect := sprite.I.Bounds()
+			rect.Max.Y = bottom - top
+			screen.DrawImage(sprite.I.SubImage(rect).(*ebiten.Image), op)
 		}
-		// last's bound is not scaled, since it's derived from the source.
-		last := sprite.I.Bounds()
-		last.Max.Y = last.Max.Y*r/h + 1
-		screen.DrawImage(sprite.I.SubImage(last).(*ebiten.Image), op)
 	}
 }
 
