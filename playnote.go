e39ef599397bd3be380512ee11305a95d5d63a07
Muang <hndada@dgist.ac.kr>
2022-08-13 01:08:17 +0900

    Change NotePosition() to Position()
    may use in non-note object's position

--- playnote.go
+++ playnote.go
@@ -48,12 +48,12 @@ func NewPlayNotes(c *Chart) ([]*PlayNote, []*PlayNote, []*PlayNote) {
 
 // top returns top position of long note body.
 func (s ScenePlay) Top(tail *PlayNote) int {
-	return int(s.NotePosition(tail)+s.TailSprites[tail.Key].H/2) - 1 // Extra 1 pixel for compensating round-down
+	return int(s.Position(tail.Time)+s.TailSprites[tail.Key].H/2) - 1 // Extra 1 pixel for compensating round-down
 }
 
 // bottom returns bottom position of long note body.
 func (s ScenePlay) Bottom(tail *PlayNote) int {
-	return int(s.NotePosition(tail.Prev)-s.HeadSprites[tail.Key].H/2) + 1 // Extra 1 pixel for compensating round-down
+	return int(s.Position(tail.Prev.Time)-s.HeadSprites[tail.Key].H/2) + 1 // Extra 1 pixel for compensating round-down
 }
 
 // DrawLongNotes draws long sprite with Binary-building method.
@@ -126,7 +126,7 @@ func (s *ScenePlay) DrawNotes(screen *ebiten.Image) {
 		default:
 			sprite = s.NoteSprites[n.Key]
 		}
-		sprite.Y = s.NotePosition(n) - sprite.H/2
+		sprite.Y = s.Position(n.Time) - sprite.H/2
 		op := sprite.Op()
 		if n.Type == Head {
 			if n.Next.Marked {
@@ -144,27 +144,27 @@ func (s *ScenePlay) DrawNotes(screen *ebiten.Image) {
 // NotePosition calculates position, the centered y-axis value.
 // y = position - h/2
 // Todo: kinda laggy on intro at Runengon Lenfried's map?
-func (s ScenePlay) NotePosition(n *PlayNote) float64 {
+func (s ScenePlay) Position(time int64) float64 {
 	var distance float64 // Approaching notes have positive distance, vice versa.
 	tp := s.TransPoint
-	time := s.Time()
+	cursor := s.Time()
 	bpmRatio := tp.BPM / s.MainBPM
-	if n.Time-s.Time() > 0 {
+	if time-s.Time() > 0 {
 		// When there are more than 2 TransPoint in bounded time.
-		for ; tp.Next != nil && tp.Next.Time < n.Time; tp = tp.Next {
-			duration := tp.Next.Time - time
+		for ; tp.Next != nil && tp.Next.Time < time; tp = tp.Next {
+			duration := tp.Next.Time - cursor
 			distance += s.Speed * (bpmRatio * tp.SpeedFactor) * float64(duration)
-			time += duration
+			cursor += duration
 		}
 	} else {
-		for ; tp.Prev != nil && tp.Time > n.Time; tp = tp.Prev {
-			duration := tp.Time - time // Negative value.
+		for ; tp.Prev != nil && tp.Time > time; tp = tp.Prev {
+			duration := tp.Time - cursor // Negative value.
 			distance += s.Speed * (bpmRatio * tp.SpeedFactor) * float64(duration)
-			time += duration
+			cursor += duration
 		}
 	}
 	// Calculate the remained speed factor (which is farthest from Hint within bound.)
-	distance += s.Speed * (bpmRatio * tp.SpeedFactor) * float64(n.Time-time)
+	distance += s.Speed * (bpmRatio * tp.SpeedFactor) * float64(time-cursor)
 	return HintPosition - distance
 }
 
