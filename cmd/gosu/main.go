986d1c802bcb1de48e0d96d433e7526cb285682b
Muang <hndada@dgist.ac.kr>
2022-08-21 16:45:05 +0900

    move game.go to mode
    With brilliant idea: assign mode-specific function at the very last: at cmd

--- cmd/gosu/main.go
+++ cmd/gosu/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hndada/gosu"
+	"github.com/hndada/gosu/mode"
+	"github.com/hndada/gosu/mode/piano"
 )
 
 func main() {
-	g := gosu.NewGame()
+	g := mode.NewGame([]mode.Mode{piano.ModePiano4, piano.ModePiano7})
 	if err := ebiten.RunGame(g); err != nil {
 		panic(err)
 	}
