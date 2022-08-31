e6ff7126687f0a2b5c3e9b37de9f1bac376516c0
Muang <hndada@dgist.ac.kr>
2022-08-31 14:27:09 +0900

    Tidy up

--- settings.go
+++ settings.go
@@ -1,34 +1,29 @@
 package gosu
 
-// Todo: BarLine color settings
-var (
-	TPS    = 1000 // TPS should be 1000 or greater.
-	Volume = 0.25
-
-	MeterWidth  float64 = 4 // The number of pixels per 1ms.
-	MeterHeight float64 = 50
-	CursorScale float64 = 0.1
-
-	BgDimness float64 = 0.5
-
-	ChartInfoBoxWidth  float64 = 450
-	ChartInfoBoxHeight float64 = 50
-	ChartInfoBoxShrink float64 = 0.15
-
-	chartInfoBoxshrink float64 = ChartInfoBoxWidth * ChartInfoBoxShrink
-
-	ScoreScale float64 = 0.65
-)
-
 var (
 	MusicRoot   = "music"
 	WindowSizeX = 1600
 	WindowSizeY = 900
 )
 
+// Todo: BarLine color settings
+var (
+	TPS    = 1000 // TPS should be 1000 or greater.
+	Volume = 0.25
+
+	ChartInfoBoxWidth  float64 = 450
+	ChartInfoBoxHeight float64 = 50
+	ChartInfoBoxShrink float64 = 0.15
+	chartInfoBoxshrink float64 = ChartInfoBoxWidth * ChartInfoBoxShrink
+
+	BackgroundDimness float64 = 0.5
+	MeterWidth        float64 = 4 // The number of pixels per 1ms.
+	MeterHeight       float64 = 50
+	CursorScale       float64 = 0.1
+	ScoreScale        float64 = 0.65
+)
+
 // Todo: reset all tick-dependent variables.
 // They are mostly at drawer.go or play.go, settings.go
 // Keyword: TimeToTick
-func SetTPS() {
-
-}
+func SetTPS() {}
