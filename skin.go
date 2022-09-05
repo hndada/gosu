5055c15cf2e341cd3235e6af7902290c7fbbc29a
Muang <hndada@dgist.ac.kr>
2022-09-05 17:14:23 +0900

    make sprite.SetScale() simpler
    now the function has only 1 input parameter.
    SetScale() -> SetScaleXY()

--- skin.go
+++ skin.go
@@ -24,6 +24,7 @@ var (
 
 	ScoreSprites [10]draws.Sprite
 	SignSprites  [3]draws.Sprite
+	// ComboSprites [10]draws.Sprite
 )
 
 const (
@@ -38,7 +39,7 @@ func LoadGeneralSkin() {
 	names := []string{"menu-cursor", "menu-cursor-additive", "cursortrail"}
 	for i, name := range names {
 		s := draws.NewSprite(fmt.Sprintf("skin/cursor/%s.png", name))
-		s.SetScale(CursorScale, CursorScale, ebiten.FilterLinear)
+		s.SetScale(CursorScale)
 		s.SetPosition(screenSizeX/2, screenSizeY/2, draws.OriginCenter)
 		CursorSprites[i] = s
 	}
@@ -46,13 +47,13 @@ func LoadGeneralSkin() {
 		s := draws.NewSprite("skin/box.png")
 		scaleW := ChartInfoBoxWidth / s.W()
 		scaleH := ChartInfoBoxHeight / s.H()
-		s.SetScale(scaleW, scaleH, ebiten.FilterLinear)
+		s.SetScaleXY(scaleW, scaleH, ebiten.FilterLinear)
 		s.SetPosition(screenSizeX+chartInfoBoxshrink, screenSizeY/2, draws.OriginRightCenter)
 		ChartInfoBoxSprite = s
 	}
 	for i := 0; i < 10; i++ {
 		s := draws.NewSprite(fmt.Sprintf("skin/score/%d.png", i))
-		s.SetScale(ScoreScale, ScoreScale, ebiten.FilterLinear)
+		s.SetScale(ScoreScale)
 		if i == 0 {
 			s.SetPosition(screenSizeX, 0, draws.OriginRightTop)
 		} else { // Need to set same base line, since each number has different height.
@@ -62,15 +63,14 @@ func LoadGeneralSkin() {
 	}
 	for i, name := range []string{"dot", "comma", "percent"} {
 		s := draws.NewSprite(fmt.Sprintf("skin/score/%s.png", name))
-		s.SetScale(ScoreScale, ScoreScale, ebiten.FilterLinear)
+		s.SetScale(ScoreScale)
 		s.SetPosition(screenSizeX, 0, draws.OriginRightTop)
 		SignSprites[i] = s
 	}
 }
 func NewBackground(path string) draws.Sprite {
 	s := draws.NewSprite(path)
-	scale := screenSizeX / s.W()
-	s.SetScale(scale, scale, ebiten.FilterLinear)
+	s.SetScale(screenSizeX / s.W())
 	s.SetPosition(screenSizeX/2, screenSizeY/2, draws.OriginCenter)
 	return s
 }
