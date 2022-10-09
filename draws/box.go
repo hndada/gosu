3fb5664c8b5434d52250698c8676cd8a74e83378
Muang <hndada@dgist.ac.kr>
2022-10-10 00:48:55 +0900

    in dev: Box, Align, Origin

--- draws/box.go
+++ draws/box.go
@@ -4,16 +4,41 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// Box model
 // Box is wrapped Subject(Sprite, Label) with Position data.
-// x and y of Sub box are determined by Outer box.
 type Box struct {
 	Subject
 	Point
 	Origin
+
+	Subs    [][]Box
+	Padding Point
+	Margin  Point
 	Align
-	Pad  Point
-	Gap  Point // Margin
-	Subs [][]Box
+}
+
+// Zero-valued Point infers tight box.
+func (b Box) mins() [][]Point {
+
+}
+
+// x and y of Sub box are determined by Outer box.
+// Suppose boxes in a same row has equal height.
+// Wait, no, labels have different height.
+func (b Box) Draw(screen *ebiten.Image, op ebiten.DrawImageOptions) {
+	b.Subject.Draw(screen, op, b.Min())
+	var offset Point
+	for _, row := range b.Subs {
+		offset.X = 0
+		offset.Y += b.Pad.Y
+		for _, sub := range row {
+			offset.X += b.Pad.X
+			sub.Point.Add(offset)
+			sub.Draw(screen, op)
+			offset.X += sub.Subject.Size().X
+		}
+		offset.Y += row[0].Subject.Size().Y
+	}
 }
 
 func (b Box) Min() Point {
@@ -37,27 +62,6 @@ func (b Box) Min() Point {
 	}
 	return p
 }
-func (b Box) Max() Point {
-	p := b.Point
-	w, h := b.Size().XY()
-	switch b.Origin.PositionX() {
-	case OriginLeft:
-		p.X += w
-	case OriginCenter:
-		p.X += w / 2
-	case OriginRight:
-		p.X += 0
-	}
-	switch b.Origin.PositionY() {
-	case OriginTop:
-		p.Y += h
-	case OriginMiddle:
-		p.Y += h / 2
-	case OriginBottom:
-		p.Y += 0
-	}
-	return p
-}
 
 // Input is usually cursor's position.
 func (b Box) In(p Point) bool {
@@ -65,9 +69,25 @@ func (b Box) In(p Point) bool {
 	w, h := b.Size().XY()
 	return p.X >= 0 && p.X <= w && p.Y >= 0 && p.Y <= h
 }
-func (b Box) Draw(screen *ebiten.Image, op ebiten.DrawImageOptions) {
-	b.Subject.Draw(screen, op, b.Min())
-}
 
-// https://www.w3schools.com/css/css_grid.asps
-// Star model: Prev, Next, Parent, First/Last Child
+// func (b Box) Max() Point {
+// 	p := b.Point
+// 	w, h := b.Size().XY()
+// 	switch b.Origin.PositionX() {
+// 	case OriginLeft:
+// 		p.X += w
+// 	case OriginCenter:
+// 		p.X += w / 2
+// 	case OriginRight:
+// 		p.X += 0
+// 	}
+// 	switch b.Origin.PositionY() {
+// 	case OriginTop:
+// 		p.Y += h
+// 	case OriginMiddle:
+// 		p.Y += h / 2
+// 	case OriginBottom:
+// 		p.Y += 0
+// 	}
+// 	return p
+// }
--- draws/subject.go
+++ draws/subject.go
@@ -10,48 +10,70 @@ import (
 
 type Subject interface {
 	Size() Point
+	SetSize(size Point)
 	Draw(*ebiten.Image, ebiten.DrawImageOptions, Point)
 }
 
-// Sprite is scaled image.
+// Sprite is a scaled image.
 type Sprite3 struct {
 	i     *ebiten.Image
 	Scale Point
+	// Scale_1 Point
 }
 
+func NewSprite3(path string) Sprite3 {
+	return Sprite3{NewImage(path), Point{1, 1}}
+}
+func NewSprite3FromImage(i *ebiten.Image) Sprite3 {
+	return Sprite3{i, Point{1, 1}}
+}
 func (s Sprite3) Size() Point {
+	// scale := s.Scale_1.Add(Pt(1, 1))
+	// return Pt(s.i.Size()).Mul(scale)
 	return Pt(s.i.Size()).Mul(s.Scale)
+}
+func (s *Sprite3) SetSize(size Point) {
+	// scale := size.Div(Pt(s.i.Size()))
+	// s.Scale_1 = scale.Sub(Pt(1, 1))
+	s.Scale = size.Div(Pt(s.i.Size()))
 }
 
 // Currently option's Filter is fixed with FilterLinear.
 func (s Sprite3) Draw(screen *ebiten.Image, op ebiten.DrawImageOptions, p Point) {
+	// scale := s.Scale_1.Add(Pt(1, 1))
+	// op.GeoM.Scale(scale.XY())
 	op.GeoM.Scale(s.Scale.XY())
 	op.GeoM.Translate(p.XY())
 	op.Filter = ebiten.FilterLinear
 	screen.DrawImage(s.i, &op)
 }
 
-// func (s *Sprite3) SetScale(scale float64) {
-// 	s.SetScaleWH(scale, scale)
-// }
-// func (s *Sprite3) SetScaleWH(scaleW, scaleH float64) {
-// 	s.Scale.W *= scaleW
-// 	s.Scale.H *= scaleH
-// }
-
 // Label may have expecting w and h by selecting specific Face.
 type Label struct {
 	Text  string
 	Face  font.Face
 	Color color.Color
+	Scale Point
 }
 
+func NewLabel(text string, face font.Face, color color.Color) Label {
+	return Label{
+		Text:  text,
+		Face:  face,
+		Color: color,
+		Scale: Point{1, 1},
+	}
+}
 func (l Label) Size() Point {
 	b := text.BoundString(l.Face, l.Text)
-	return Pt(b.Max.X, -b.Min.Y)
+	return Pt(b.Max.X, -b.Min.Y).Mul(l.Scale)
+}
+func (l *Label) SetSize(size Point) {
+	l.Scale = size.Div(l.Size())
 }
 
 func (l Label) Draw(screen *ebiten.Image, op ebiten.DrawImageOptions, p Point) {
+	op.GeoM.Scale(l.Scale.XY())
 	op.GeoM.Translate(p.XY())
 	text.DrawWithOptions(screen, l.Text, l.Face, &op)
 }
