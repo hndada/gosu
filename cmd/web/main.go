d6eb42ed00ffc3846d971ab64c59205364159591
Muang <hndada@dgist.ac.kr>
2022-10-17 02:16:12 +0900

    browser basically does not support file system

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,8 +7,14 @@ import (
 	"github.com/hndada/gosu/mode/piano"
 )
 
+// //go:embed skin
+// var skin embed.FS
+
+// //go:embed music
+// var music embed.FS
+
 func main() {
-	g := gosu.NewGame([]gosu.ModeProp{piano.ModePiano4, piano.ModePiano7, drum.ModeDrum})
+	g := gosu.NewGame([]gosu.ModeProp{piano.ModePiano4, piano.ModePiano7, drum.ModeDrum}) //, skin, music)
 	if err := ebiten.RunGame(g); err != nil {
 		panic(err)
 	}
