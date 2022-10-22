a06f47c6efdd2bfe0610b3f27f8b58665f805df5
Muang <hndada@dgist.ac.kr>
2022-10-22 15:10:21 +0900

    test push for automated build via tags

--- chartinfo.go
+++ chartinfo.go
@@ -48,6 +48,25 @@ func (c ChartInfo) NoteCountString() string {
 	return fmt.Sprintf("◎ %d", c.NoteCounts[0])
 }
 
+// func (c ChartInfo) Download() error {
+// 	const noVideo = 1
+// 	u := fmt.Sprintf("%s%d?n=%d", chimuURLDownload, r.SetId, noVideo)
+// 	fmt.Printf("download URL: %s\n", u)
+// 	resp, err := http.Get(u)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	f, err := os.Create(filepath.Join(dir, r.Filename()))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+
+// 	_, err = io.Copy(f, resp.Body)
+// }
+
 // Background brightness at Song select: 60% (153 / 255), confirmed.
 // Score box color: Gray128 with 50% transparent
 // Hovered Score box color: Gray96 with 50% transparent
