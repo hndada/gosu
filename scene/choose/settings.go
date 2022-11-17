bcb956e3bfda6dc0c402efadceefcb291a3e521b
Muang <hndada@dgist.ac.kr>
2022-11-17 10:45:05 +0900

    refactor loading settings, skin and sounds
    Sounds -> SoundBag
    Introduce simplest group condition
    Set min max bound values when loading settings

--- scene/choose/settings.go
+++ scene/choose/settings.go
@@ -1,16 +1,1 @@
 package choose
-
-import "github.com/hndada/gosu/scene"
-
-const ()
-
-var (
-	BackgroundBrightness = 0.6
-
-	ChartInfoBoxWidth  float64 = 450
-	ChartInfoBoxHeight float64 = 50
-	ChartInfoBoxShrink float64 = 0.15
-
-	chartInfoBoxshrink float64 = ChartInfoBoxWidth * ChartInfoBoxShrink
-	chartItemBoxCount  int     = int(scene.ScreenSizeY/ChartInfoBoxHeight) + 2 // Gives some margin.
-)
