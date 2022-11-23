9733443015a2d613561e3182a51ce423f3141169
Muang <hndada@dgist.ac.kr>
2022-11-23 09:04:10 +0900

    remove db package
    Move some useful code to choose package

--- scene/choose/scene.go
+++ scene/choose/scene.go
@@ -9,6 +9,16 @@ import (
 	"github.com/hndada/gosu/format/osr"
 )
 
+// Background brightness at Song select: 60% (153 / 255), confirmed.
+// Score box color: Gray128 with 50% transparent
+// Hovered Score box color: Gray96 with 50% transparent
+
+// https://osu.ppy.sh/docs/index.html#beatmapsetcompact-covers
+// cover, card, list, slimcover
+
+// rnkaed status
+// graveyard, wip, pending
+// ranked, approved, qualified, loved
 type Scene struct{}
 
 func NewScene() *Scene {
