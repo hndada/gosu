8cc84c7683380009b77985ea7a8e81a86d38121f
Muang <hndada@dgist.ac.kr>
2022-11-23 18:24:00 +0900

    (wip) choose scene
    chimu-driven choose scene

--- scene/choose/skin.go
+++ scene/choose/skin.go
@@ -1,17 +1,8 @@
 package choose
 
 import (
-	"io/fs"
-
 	"github.com/hndada/gosu/draws"
 	"github.com/hndada/gosu/mode"
-	"github.com/hndada/gosu/scene"
-)
-
-const (
-	TPS         = scene.TPS
-	ScreenSizeX = scene.ScreenSizeX
-	ScreenSizeY = scene.ScreenSizeY
 )
 
 type Skin struct {
@@ -34,19 +25,6 @@ var (
 	UserSkin    = Skin{Type: mode.User}
 )
 
-func (skin *Skin) Load(fsys fs.FS) {
-	skin.DefaultBackground = scene.UserSkin.DefaultBackground
-	{
-		s := draws.NewSprite(fsys, "interface/box-mask.png")
-		s.SetSize(chartBoxWidth, chartBoxHeight)
-		x := ScreenSizeX - chartBoxShrink
-		y := float64(ScreenSizeY) / 2
-		s.Locate(x, y, draws.RightMiddle)
-		skin.ChartBox = s
-	}
-	base := []Skin{{}, DefaultSkin, UserSkin}[skin.Type]
-	skin.fillBlank(base)
-}
 func (skin *Skin) fillBlank(base Skin) {
 	if !skin.DefaultBackground.IsValid() {
 		skin.DefaultBackground = base.DefaultBackground
