8fa82e2b050fb79c0083963be484deb6d313b5d5
Muang <hndada@dgist.ac.kr>
2023-03-29 13:05:16 +0900

    wip: v0.5
    objective: simplify

--- cmd/gosu/main.go
+++ cmd/gosu/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hndada/gosu"
 )
 
 func main() {
-	g := gosu.NewGame()
-	if err := ebiten.RunGame(g); err != nil {
-		panic(err)
-	}
-	// dir, err := os.Getwd()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// gosu.Load(os.DirFS(dir))
 	// g := gosu.NewGame()
 	// if err := ebiten.RunGame(g); err != nil {
 	// 	panic(err)
 	// }
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	gosu.Load(os.DirFS(dir))
+	g := gosu.NewGame()
+	if err := ebiten.RunGame(g); err != nil {
+		panic(err)
+	}
 }
