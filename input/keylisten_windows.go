7bcada3d51a0ce53c66fa56d3f93318f2fb9c140
Muang <hndada@dgist.ac.kr>
2023-07-07 21:31:45 +0900

    add KeyInputListener

--- input/keylisten_windows.go
+++ input/keylisten_windows.go
@@ -3,30 +3,51 @@
 package input
 
 import (
+	"time"
+
 	"golang.org/x/sys/windows"
 )
 
+// https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getkeystate
+// Get'Async'KeyState would make the exectuable false-positive.
 var (
-	moduser32            = windows.NewLazyDLL("user32.dll")
-	procGetAsyncKeyState = moduser32.NewProc("GetAsyncKeyState")
+	moduser32       = windows.NewLazyDLL("user32.dll")
+	procGetKeyState = moduser32.NewProc("GetKeyState")
 )
 
 const (
-	wasPressed = 0x0001 // Deprecated: whether the key was pressed after the previous call to GetAsyncKeyState
+	wasPressed = 0x0001 // Deprecated: whether the key was pressed after the previous call to GetKeyState
 	isPressed  = 0x8000
 )
 
-func NewListener(keySettings []Key) func() []bool {
-	vkcodes := make([]uint32, len(keySettings))
-	for k, ek := range keySettings {
+func NewKeyInputListener(ks []Key) *KeyInputListener {
+	const defaultPollingRate = 1 * time.Millisecond
+
+	vkcodes := make([]uint32, len(ks))
+	for k, ek := range ks {
 		vkcodes[k] = ToVirtualKey(ek)
 	}
-	return func() []bool {
+
+	listen := func() KeyInputLog {
+		now := time.Now()
 		pressed := make([]bool, len(vkcodes))
 		for k, vk := range vkcodes {
-			v, _, _ := procGetAsyncKeyState.Call(uintptr(vk))
+			v, _, _ := procGetKeyState.Call(uintptr(vk))
 			pressed[k] = v&isPressed != 0
 		}
-		return pressed
+		return KeyInputLog{
+			Time:    now,
+			Pressed: pressed,
+		}
 	}
+
+	listener := &KeyInputListener{
+		KeySettings: ks,
+		vkcodes:     vkcodes,
+		PollingRate: defaultPollingRate,
+		Listen:      listen,
+
+		Logs: make([]KeyInputLog, 0, 1000),
+	}
+	return listener
 }
