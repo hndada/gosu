5bb61efd89990f6c35ed8640d57c902f49a53488
Muang <hndada@dgist.ac.kr>
2023-12-17 14:49:07 +0900

    Tween implement done
    tweens -> Tween

--- draws/tween.go
+++ draws/tween.go
@@ -3,7 +3,18 @@ package draws
 import "math"
 
 // Tween calculates intermediate values between two values over a specified duration.
+// Yoyo is nearly no use when each tweens is not continuous.
+// Todo: Add yoyo mode?
 type Tween struct {
+	tweens  []tween
+	index   int
+	loop    int
+	maxLoop int
+	// yoyo     bool // for yoyo
+	// backward bool // for yoyo
+}
+
+type tween struct {
 	tick    int
 	begin   float64
 	change  float64
@@ -12,23 +23,7 @@ type Tween struct {
 	// backward bool // for yoyo
 }
 
-// Easing function requires 4 arguments:
-// current time (tick), begin and change values, and duration (max tick).
-type TweenFunc func(tick int, begin, change float64, maxTick int) float64
-
-func NewTween(begin, change float64, maxTick int, easing TweenFunc) Tween {
-	return Tween{
-		begin:   begin,
-		change:  change,
-		maxTick: maxTick,
-		easing:  easing,
-	}
-}
-
-// IsFinished returns false if the loop is infinite.
-func (tw Tween) IsFinished() bool { return tw.tick >= tw.maxTick }
-
-func (tw *Tween) Tick() {
+func (tw *tween) Tick() {
 	if tw.IsFinished() {
 		return
 	}
@@ -36,6 +31,58 @@ func (tw *Tween) Tick() {
 		tw.tick++
 	}
 	tw.change = tw.easing(tw.tick, tw.begin, tw.change, tw.maxTick)
+}
+
+func (tw tween) IsFinished() bool { return tw.tick >= tw.maxTick }
+
+// Easing function requires 4 arguments:
+// current time (tick), begin and change values, and duration (max tick).
+type TweenFunc func(tick int, begin, change float64, maxTick int) float64
+
+func NewTween(begin, change float64, maxTick int, easing TweenFunc) (tw Tween) {
+	tw.AppendTween(begin, change, maxTick, easing)
+	return
+}
+
+func (tw *Tween) AppendTween(begin, change float64, maxTick int, easing TweenFunc) {
+	tw.tweens = append(tw.tweens, tween{
+		begin:   begin,
+		change:  change,
+		maxTick: maxTick,
+		easing:  easing,
+	})
+}
+
+func (tw *Tween) SetLoop(maxLoop int) { tw.maxLoop = maxLoop }
+
+func (tw *Tween) Tick() {
+	if tw.IsFinished() {
+		return
+	}
+	if len(tw.tweens) == 0 {
+		return
+	}
+
+	// Process the current Tween
+	tw.tweens[tw.index].Tick()
+	if !tw.tweens[tw.index].IsFinished() {
+		return
+	}
+
+	// Process the next Tween
+	if tw.index < len(tw.tweens)-1 {
+		tw.index++
+	} else {
+		tw.loop++
+		if tw.loop < tw.maxLoop {
+			tw.index = 0
+		}
+	}
+}
+
+// IsFinished returns false if the loop is infinite.
+func (tw Tween) IsFinished() bool {
+	return tw.maxLoop != 0 && tw.loop >= tw.maxLoop
 }
 
 // Easing functions
@@ -55,93 +102,4 @@ func EaseOutExponential(tick int, begin, change float64, maxTick int) float64 {
 	k := math.Log(math.Abs(change)) // steepness
 	dx := float64(tick) / float64(maxTick)
 	return begin + change*(1-math.Exp(-k*dx))
-}
-
-// Yoyo is nearly no use when each tweens is not continuous.
-// Hence, yoyo is implemented in Tweens only, not in Tween.
-// Loop is also implemented in Tweens only for readability.
-// Todo: Add yoyo mode?
-type Tweens struct {
-	Tweens []Tween
-	index  int
-
-	loop    int
-	maxLoop int
-	// yoyo     bool // for yoyo
-	// backward bool // for yoyo
-}
-
-func NewTweens(tws ...Tween) Tweens {
-	return Tweens{Tweens: tws}
-}
-
-func (tws *Tweens) SetLoop(maxLoop int, yoyo bool) {
-	tws.maxLoop = maxLoop
-	// tws.yoyo = yoyo
-}
-
-// IsFinished returns false if the loop is infinite.
-func (tws Tweens) IsFinished() bool {
-	return tws.maxLoop != 0 && tws.loop >= tws.maxLoop
-}
-
-// func (tws *Tweens) Reset() {
-// 	for i := range tws.Tweens {
-// 		tws.Tweens[i].Reset()
-// 	}
-// 	tws.index = 0
-// 	tws.loop = 0
-// }
-
-func (tws *Tweens) Tick() {
-	if tws.IsFinished() {
-		return
-	}
-	if len(tws.Tweens) == 0 {
-		return
-	}
-
-	// Process the current Tween
-	tws.Tweens[tws.index].Tick()
-	if !tws.Tweens[tws.index].IsFinished() {
-		return
-	}
-
-	if tws.index < len(tws.Tweens)-1 {
-		tws.index++
-	} else {
-		tws.loop++
-		if tws.loop < tws.maxLoop {
-			tws.index = 0
-		}
-	}
-
-	// // Move to the next Tween if the current one is finished
-	// switch {
-	// case !tws.yoyo:
-	// 	// Standard behavior: increment tick until maxTick
-	// 	if tws.index < len(tws.Tweens)-1 {
-	// 		tws.index++
-	// 	} else {
-	// 		tws.loop++
-	// 		if tws.loop < tws.maxLoop {
-	// 			tws.index = 0
-	// 		}
-	// 	}
-	// case tws.yoyo && !tws.backward:
-	// 	// Yoyo mode - increasing tick
-	// 	if tws.index < len(tws.Tweens)-1 {
-	// 		tws.index++
-	// 	} else {
-	// 		tws.backward = true
-	// 	}
-	// case tws.yoyo && tws.backward:
-	// 	// Yoyo mode - decreasing tick
-	// 	if tws.index > 0 {
-	// 		tws.index--
-	// 	} else {
-	// 		tws.backward = false
-	// 		tws.loop++
-	// 	}
-	// }
 }
