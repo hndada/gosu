27f9dd74ade75eff69797bcfea459182f88d8e9a
Muang <hndada@dgist.ac.kr>
2023-12-17 14:03:54 +0900

    wip: start, change, maxTick, easing

--- draws/tween.go
+++ draws/tween.go
@@ -4,108 +4,91 @@ import "math"
 
 // Tween encapsulates the easing function along with timing data.
 // It is used to animate between two values over a specified duration.
+// Todo: Add yoyo mode?
 type Tween struct {
-	start   float64
-	current float64
-	end     float64
 	tick    int
+	start   float64
+	change  float64 // end - start
 	maxTick int
 	easing  TweenFunc
-
-	loop     int
-	maxLoop  int
-	yoyo     bool // for yoyo
-	backward bool // for yoyo
+	// backward bool // for yoyo
 }
 
-type TweenFunc func(start, current, end float64, tick, maxTick int) float64
+// Easing function requires 4 arguments:
+// current time (tick), start and change values, and duration (max tick).
+type TweenFunc func(tick int, start, change float64, maxTick int) float64
 
-func NewTween(start, end float64, maxTick int, easing TweenFunc) Tween {
+func NewTween(start, change float64, maxTick int, easing TweenFunc) Tween {
 	return Tween{
 		start:   start,
-		current: start,
-		end:     end,
-		tick:    0,
+		change:  change,
 		maxTick: maxTick,
 		easing:  easing,
 	}
 }
 
-func (tw *Tween) SetLoop(maxLoop int, yoyo bool) {
-	tw.maxLoop = maxLoop
-	tw.yoyo = yoyo
-}
+// IsFinished returns false if the loop is infinite.
+func (tw Tween) IsFinished() bool { return tw.tick >= tw.maxTick }
 
-func (tw *Tween) UpdateEnd(end float64) {
-	tw.start = tw.current
-	tw.end = end
-	tw.Reset()
-}
+// func (tw *Tween) SetChange(change float64) {
+// 	tw.start = tw.start + tw.change
+// 	tw.change = change
+// 	tw.Reset()
+// }
+
+// func (tw *Tween) Reset() {
+// 	tw.tick = 0
+// 	tw.change = 0
+// }
 
 func (tw *Tween) Tick() {
 	if tw.IsFinished() {
 		return
 	}
 
-	switch {
-	case !tw.yoyo:
-		// Standard behavior: increment tick until maxTick
-		if tw.tick < tw.maxTick {
-			tw.tick++
-		} else {
-			tw.loop++
-			if tw.loop < tw.maxLoop {
-				tw.tick = 0
-			}
-		}
-	case tw.yoyo && !tw.backward:
-		// Yoyo mode - increasing tick
-		if tw.tick < tw.maxTick {
-			tw.tick++
-		} else {
-			tw.backward = true
-		}
-	case tw.yoyo && tw.backward:
-		// Yoyo mode - decreasing tick
-		if tw.tick > 0 {
-			tw.tick--
-		} else {
-			tw.backward = false
-			tw.loop++
-		}
+	if tw.tick < tw.maxTick {
+		tw.tick++
 	}
-
-	// Update the current value based on the easing function
-	tw.current = tw.easing(tw.start, tw.current, tw.end, tw.tick, tw.maxTick)
-}
-
-// IsFinished returns false if the loop is infinite.
-func (tw Tween) IsFinished() bool {
-	return tw.maxLoop != 0 && tw.loop >= tw.maxLoop
-}
-
-func (tw *Tween) Reset() {
-	tw.tick = 0
-	tw.loop = 0
-	tw.backward = false
+	tw.change = tw.easing(tw.tick, tw.start, tw.change, tw.maxTick)
 }
 
 // Tweens has separate loop variables apart from each Tween
 // because loop can be either each Tween's loop or whole Tweens' loop.
+
+// Yoyo is nearly no use when each tweens is not continuous.
+// Hence, yoyo is implemented in Tweens only, not in Tween.
+// Loop is also implemented in Tweens only for readability.
 type Tweens struct {
 	Tweens []Tween
 	index  int
 
-	loop     int
-	maxLoop  int
-	yoyo     bool // for yoyo
-	backward bool // for yoyo
+	loop    int
+	maxLoop int
+	// yoyo     bool // for yoyo
+	// backward bool // for yoyo
+}
+
+func NewTweens(tws ...Tween) Tweens {
+	return Tweens{Tweens: tws}
 }
 
 func (tws *Tweens) SetLoop(maxLoop int, yoyo bool) {
 	tws.maxLoop = maxLoop
-	tws.yoyo = yoyo
+	// tws.yoyo = yoyo
 }
+
+// IsFinished returns false if the loop is infinite.
+func (tws Tweens) IsFinished() bool {
+	return tws.maxLoop != 0 && tws.loop >= tws.maxLoop
+}
+
+// func (tws *Tweens) Reset() {
+// 	for i := range tws.Tweens {
+// 		tws.Tweens[i].Reset()
+// 	}
+// 	tws.index = 0
+// 	tws.loop = 0
+// }
 
 func (tws *Tweens) Tick() {
 	if tws.IsFinished() {
@@ -117,45 +100,65 @@ func (tws *Tweens) Tick() {
 
 	// Process the current Tween
 	tws.Tweens[tws.index].Tick()
-	tw := tws.Tweens[tws.index]
-	if !tw.IsFinished() {
+	if !tws.Tweens[tws.index].IsFinished() {
 		return
 	}
 
-	// Move to the next Tween if the current one is finished
-	tws.index++
-	// Handle the wrapping of the index and update the loop count
-	if tws.index >= len(tws.Tweens) {
-		tws.index = 0
+	if tws.index < len(tws.Tweens)-1 {
+		tws.index++
+	} else {
 		tws.loop++
-		// In yoyo mode, reverse the direction of each Tween
-		if tws.yoyo {
-			for i := range tws.Tweens {
-				tws.Tweens[i].backward = !tws.Tweens[i].backward
-			}
+		if tws.loop < tws.maxLoop {
+			tws.index = 0
 		}
 	}
-}
 
-func (tws Tweens) IsFinished() bool {
-	return tws.maxLoop != 0 && tws.loop >= tws.maxLoop
-}
-
-func (tws *Tweens) Reset() {
-	for i := range tws.Tweens {
-		tws.Tweens[i].Reset()
-	}
-	tws.loop = 0
-	tws.backward = false
+	// // Move to the next Tween if the current one is finished
+	// switch {
+	// case !tws.yoyo:
+	// 	// Standard behavior: increment tick until maxTick
+	// 	if tws.index < len(tws.Tweens)-1 {
+	// 		tws.index++
+	// 	} else {
+	// 		tws.loop++
+	// 		if tws.loop < tws.maxLoop {
+	// 			tws.index = 0
+	// 		}
+	// 	}
+	// case tws.yoyo && !tws.backward:
+	// 	// Yoyo mode - increasing tick
+	// 	if tws.index < len(tws.Tweens)-1 {
+	// 		tws.index++
+	// 	} else {
+	// 		tws.backward = true
+	// 	}
+	// case tws.yoyo && tws.backward:
+	// 	// Yoyo mode - decreasing tick
+	// 	if tws.index > 0 {
+	// 		tws.index--
+	// 	} else {
+	// 		tws.backward = false
+	// 		tws.loop++
+	// 	}
+	// }
 }
 
 // Easing functions
-func EaseLinear(start, current, end float64, tick, maxTick int) float64 {
-	return current + (end-start)/float64(maxTick)
+func EaseLinear(start, end, current float64, maxTick int) float64 {
+	return (end - start) / float64(maxTick)
 }
 
-func EaseOutExponential(start, current, end float64, tick, maxTick int) float64 {
-	diff := math.Abs(end - start)
-	factor := 1 - math.Exp(-math.Log(diff)/float64(maxTick))
-	return current + (end-start)*factor
+// y = start + (end-start) * (1-exp(-k/x))
+func EaseOutExponential(tick int, start, change float64, maxTick int) float64 {
+	// Decayed.go
+	// k := math.Log(math.Abs(end - start)) // steepness
+	// factor := 1 - math.Exp(-k/float64(maxTick))
+	// return (end - start) * factor
+
+	if tick >= maxTick {
+		return start + change
+	}
+	k := 10.0 // Constant for exponential decay rate
+	return start + change*(-math.Pow(2, -k*float64(tick)/float64(maxTick))+1)
+
 }
